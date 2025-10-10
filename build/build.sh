ce0d271d6390611f5895460dab5b1170bb8a7ad7
tbpaksoy <tahsin.basar.paksoy.019@gmail.com>
2025-10-10 13:52:35 +0300

    Set build.sh

---
+++ build/build.sh
@@ -0,0 +1,6 @@
+for file in ../src/**/*.cpp ../src/*.cpp; do
+    echo "Compiling $file"
+    clang++ -std=c++17 -g -I../src -I/usr/include -I/home/tahsin/C++Libs/poly2tri -c "$file"
+done
+
+ar rcs libgpp.a *.o
