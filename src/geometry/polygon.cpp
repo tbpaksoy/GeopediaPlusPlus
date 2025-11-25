6415891ec601b2b3fef107114b1853dde6f7a062
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2025-11-25 12:30:27 +0300

    Updated polygon class.

--- src/geometry/polygon.cpp
+++ src/geometry/polygon.cpp
@@ -1,9 +1,29 @@
 #include "polygon.h"
 #include <poly2tri/poly2tri.h>
 
+Polygon::Polygon()
+{
+}
+
 Polygon::Polygon(std::vector<glm::vec2> vertices)
 {
     this->vertices = vertices;
+}
+
+void Polygon::AddVertex(const glm::vec2 &vertex)
+{
+    vertices.push_back(vertex);
+}
+
+void Polygon::RemoveVertex(const glm::vec2 &vertex)
+{
+    vertices.erase(std::remove(vertices.begin(), vertices.end(), vertex), vertices.end());
+}
+
+void Polygon::RemoveVertex(unsigned int index)
+{
+    if (index < vertices.size())
+        vertices.erase(vertices.begin() + index);
 }
 
 std::vector<glm::vec2> Polygon::Vertices() const
@@ -13,27 +33,28 @@ std::vector<glm::vec2> Polygon::Vertices() const
 
 std::vector<unsigned int> Polygon::Indices() const
 {
-    if(indices.size())
+    if (indices.size())
         return indices;
-    else throw std::runtime_error("Polygon not triangulated yet.");
+    else
+        throw std::runtime_error("Polygon not triangulated yet.");
 }
 
 void Polygon::Triangulate()
 {
-    std::vector<p2t::Point*> polyline;
-    for (const glm::vec2& v : vertices)
+    std::vector<p2t::Point *> polyline;
+    for (const glm::vec2 &v : vertices)
         polyline.push_back(new p2t::Point(v.x, v.y));
-    
+
     p2t::CDT cdt(polyline);
     cdt.Triangulate();
 
-    std::vector<p2t::Triangle*> triangles = cdt.GetTriangles();
+    std::vector<p2t::Triangle *> triangles = cdt.GetTriangles();
     indices.clear();
-    for (p2t::Triangle* t : triangles)
+    for (p2t::Triangle *t : triangles)
     {
         for (int i = 0; i < 3; i++)
         {
-            p2t::Point* p = t->GetPoint(i);
+            p2t::Point *p = t->GetPoint(i);
             for (size_t j = 0; j < vertices.size(); j++)
             {
                 if (vertices[j].x == p->x && vertices[j].y == p->y)
@@ -44,7 +65,7 @@ void Polygon::Triangulate()
             }
         }
     }
-    while(polyline.size())
+    while (polyline.size())
     {
         delete polyline.back();
         polyline.pop_back();
--- src/geometry/polygon.h
+++ src/geometry/polygon.h
@@ -11,9 +11,14 @@ class Polygon : public Object2d
 private:
     std::vector<glm::vec2> vertices;
     std::vector<unsigned int> indices;
-public:
 
+public:
+    Polygon();
     Polygon(std::vector<glm::vec2> vertices);
+
+    void AddVertex(const glm::vec2 &vertex);
+    void RemoveVertex(const glm::vec2 &vertex);
+    void RemoveVertex(unsigned int index);
 
     std::vector<glm::vec2> Vertices() const;
     std::vector<unsigned int> Indices() const;
