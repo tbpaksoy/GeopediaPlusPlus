b1023baed09861d75a65209404969936832aedb8
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2025-11-25 12:28:38 +0300

    Shape typedefs for 3d space added.

---
+++ src/geometry/shape3d.h
@@ -0,0 +1,11 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <tuple>
+#include <glm/glm.hpp>
+
+typedef glm::vec3 Point3D;
+typedef std::tuple<glm::vec3, glm::vec3> Line3D;
+typedef std::tuple<glm::vec3, glm::vec3, glm::vec3> Triangle3D;
+
+#endif
