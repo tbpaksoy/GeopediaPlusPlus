6ff91025dde5e613aedac76ec5f2b0336cc5592b
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2025-11-28 20:52:48 +0300

    Standard.fs update. (Still has some problems and needs testing.)

--- shader/standard.fs
+++ shader/standard.fs
@@ -8,13 +8,31 @@ struct BasicLight {
 };
 
 in vec3 Color;
+in vec3 FragPos;
 
 out vec4 fragColor;
 
 #define MAX_LIGHTS 4
 uniform BasicLight lights[MAX_LIGHTS];
+uniform bool useLighting;
 
 void main() 
 {
-    fragColor = vec4(Color, 1.0);
+    if (useLighting)
+    {
+    float light = 0.0;
+        for (int i = 0; i < MAX_LIGHTS; i++) 
+        {
+            float dist = distance(FragPos, lights[i].position);
+            if (dist < lights[i].radius) 
+            {
+                light += lights[i].intensity * (1.0 - (dist / lights[i].radius));
+            }
+        }
+        fragColor = vec4(Color * light, 1.0);
+    }
+    else
+    {
+        fragColor = vec4(Color, 1.0);
+    }
 }
